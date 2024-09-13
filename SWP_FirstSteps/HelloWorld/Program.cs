028260d73aabeaf7b43b81244e13d0018eb0cc90
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-09-13 13:19:48 +0200

    HelloWorld // made User Input for numbers

---
+++ SWP_FirstSteps/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+string hello = Console.ReadLine();
+
+Console.WriteLine(hello); 
+
+int  zahl = Int32.Parse(hello);
+Console.WriteLine(zahl + 100);
