b1a7a1fb9cf60932348ba5b678db717e3f5b6270
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-09-26 19:46:29 +0200

    Programmierauftrag 2 // finished

--- SWP_FirstSteps/HelloWorld/Program.cs
+++ SWP_FirstSteps/HelloWorld/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 
-
+// Programmieraufgabe 1
 bool isInput = true;
 
 Console.WriteLine("Schreibe etwas rein!");
@@ -11,10 +12,19 @@ Console.WriteLine("Schreibe etwas rein!");
 while (isInput)
 {
     string input = Console.ReadLine();
-    Console.WriteLine(input);
-    if(input == "Beenden")
+    if (int.TryParse(input, out int output))
+    {
+        int input2 = int.Parse(input);
+        Console.WriteLine("Deine Zahl wurde um 1 vergrößert: ");
+        Console.Write(input2 +1);
+    }
+
+
+    if (input == "Beenden")
     {
         isInput = false;
     }
+
 }
+
 
