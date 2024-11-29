e4d43dc75cc07468532d571fbf5d7ea4b43da836
FreeOnur <144993689+FreeOnur@users.noreply.github.com>
2024-11-29 11:43:51 +0100

    Lebewesen//Switched the Attributes to the right class

--- SWP_FirstSteps/MyFirstCat/Cat.cs
+++ SWP_FirstSteps/MyFirstCat/Cat.cs
@@ -10,14 +10,11 @@ namespace MyFirstCat
 {
     class Cat : Animal
     {
-        
-        public string Color  { get; set; }
-        public int Age { get { return (DateTime.Now.Year - _BirthDate.Year); } }
 
-        public Cat(DateTime birthDate,string color = "Nicht Verfügbar", string name = "nicht Verfügbar") : base(name,birthDate)
+
+        public Cat(DateTime birthDate, string color = "Nicht Verfügbar", string name = "nicht Verfügbar") : base(name, birthDate, color)
         {
-            
-            Color = color;          
+
         }
         public override string ToString()
         {
--- SWP_FirstSteps/MyFirstCat/Lebewesen.cs
+++ SWP_FirstSteps/MyFirstCat/Lebewesen.cs
@@ -8,12 +8,16 @@ namespace MyFirstCat
 {
     class Lebewesen
     {
-        public DateTime _BirthDate { get; set; }
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age { get { return (DateTime.Now.Year - BirthDate.Year); } }
 
-        public Lebewesen(DateTime birthDate)
+        public Lebewesen(DateTime birthDate, string color, string name)
         {
-            
-            _BirthDate = birthDate;
+            Color = color;
+            BirthDate = birthDate;
+            Name = name;
         }
     }
 }
