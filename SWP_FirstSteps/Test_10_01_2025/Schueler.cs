33c2fe7f037a5738efa4b13283762a738f51ceeb
FreeOnur <144993689+FreeOnur@users.noreply.github.com>
2025-01-17 13:55:01 +0100

    dad

--- SWP_FirstSteps/Test_10_01_2025/Schueler.cs
+++ SWP_FirstSteps/Test_10_01_2025/Schueler.cs
@@ -50,9 +50,9 @@ namespace Test_10_01_2025
                 Console.WriteLine($"Klasse {klasse}: {anzahl} Schüler");
             }
         }
-        public Schueler(string klasse, DateTime geburttag, string geschlecht) : base(geburttag, geschlecht)
+        public Schueler(string klasse, DateTime geburttasg, string geschlecht) : base(geburttasg, geschlecht)
         {
-            Geburtstag = geburttag;
+            Geburtstag = geburttasg;
             Klasse = klasse;
             AddKlasse(klasse);
         }
